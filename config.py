8d85c8fb6a08c963e44a80864916550be618b61f
Vip-HackerZ <ommore2021107@gmail.com>
2024-11-03 18:37:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id =546431313
-api_hash = ""
-bot_token = "aaf4235465a4r3a1f3af13af1a4fa"
-auth_users = "46513214"
+api_id =18156248
+api_hash = "db946fb6805b1a698c679626b617e77a"
+bot_token = "7504169790:AAGzS8YTC9BCTfIRRtEU-ray7pEqndrlw5s"
+auth_users = "2032347579"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "56416564654"
+osowner_users = "2032347579"
 owner_users = [int(num) for num in osowner_users.split(",")]
